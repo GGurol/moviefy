65b57027ad26a658bffa8bd4c8317b9b2eb27ee0
Nicolas Leigh <linze201@gmail.com>
2025-02-21 19:10:47 +0800

    change js to ts

---
+++ frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.js";
+import "./index.css";
+import ContextProviders from "./context/index.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ContextProviders>
+        <App />
+      </ContextProviders>
+    </BrowserRouter>
+  </React.StrictMode>
+);
